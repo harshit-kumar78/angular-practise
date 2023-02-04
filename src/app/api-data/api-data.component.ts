1fe929d4f8469c563cf5ba958724d667636feb70
harshit-kumar78 <harshit3551@gmail.com>
2023-02-04 11:18:27 +0530

    observable added

--- src/app/api-data/api-data.component.ts
+++ src/app/api-data/api-data.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-api-data',
   templateUrl: './api-data.component.html',
   styleUrls: ['./api-data.component.css'],
 })
 export class ApiDataComponent {
-  userdata: any;
+  // userdata: any;
+  userdata$!: Observable<any>;
+
   constructor(private apiService: ApiService) {
-    this.apiService.getUserData().subscribe((data) => {
-      this.userdata = data;
-      console.log(this.userdata);
-    });
+    // this.apiService.getUserData().subscribe((data) => {
+    //   this.userdata = data;
+    //   console.log(this.userdata);
+    // });
+
+    let data = this.apiService.getUserData();
+    this.userdata$ = data;
+    // console.log(this.userdata$);
   }
 }
