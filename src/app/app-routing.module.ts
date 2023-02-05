71bacbdc51d1d3d36eb10bd57532830489f7cc56
harshit-kumar78 <harshit3551@gmail.com>
2023-02-05 15:32:01 +0530

    redirecting routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,9 +11,14 @@ const routes: Routes = [
     path: 'login',
     component: LoginComponent,
   },
+  //   {
+  //     path: 'about',
+  //     component: AboutComponent,
+  //   },
   {
     path: 'about',
-    component: AboutComponent,
+    redirectTo: 'login',
+    pathMatch: 'full',
   },
   {
     path: '',
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,13 +9,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AppComponent {
   controlForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private routeR: Router) {
     this.controlForm = this.fb.group({
       email: ['', Validators.required],
       password: [''],
     });
   }
-
+  onClick() {
+    this.routeR.navigate(['signup']);
+  }
   submit(val: any) {
     console.log(val);
   }
